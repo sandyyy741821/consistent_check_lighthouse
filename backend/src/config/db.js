e484aee9b362b4d95c0cc3a565edd74bc0f99f0a
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-07-25 16:01:19 +0530

    authenticate checks

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT, 10),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
